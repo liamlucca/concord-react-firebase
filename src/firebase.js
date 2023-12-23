4660923d5ac63ced257d5d2eaaf2f7a4a51224b7
liamlucca <liamlucca@users.noreply.github.com>
2023-12-23 14:17:53 -0300

    arreglos: .env + pagina de bienvenida
    -se puso la configuración del firebase.js en un .env
    -se arregló el espacio en blanco en la página de bienvenida

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCuNN4lvqVdWF5x4IWklA1znXx1B3LfmZE",
-  authDomain: "concord-chat-proyect.firebaseapp.com",
-  projectId: "concord-chat-proyect",
-  storageBucket: "concord-chat-proyect.appspot.com",
-  messagingSenderId: "126739114944",
-  appId: "1:126739114944:web:76c9b6284a90152c67f625"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
